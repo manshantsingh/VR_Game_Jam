ebde840cdb12e0a75a42183ece515840cdf398b7
FocusBlast <me@manshantsingh.com>
2015-03-04 19:16:19 -0800

    Small changes
    +Made speed public for inspector edit
    +fixed y-axis so it doesn't go down for now.

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour {
 	const float MAX_SPEED_MULTIPLIER = 15f;
 
 	Rigidbody rigidBody;
+
+    public float speed;
 	
 	void Start () {
 		rigidBody = this.GetComponent<Rigidbody> ();
@@ -13,12 +15,13 @@ public class Player : MonoBehaviour {
 
 	void FixedUpdate () {
 
-		Vector3 headRotation = this.transform.FindChild ("Head").rotation.eulerAngles; // use rotation for speed
+		//Vector3 headRotation = this.transform.FindChild ("Head").rotation.eulerAngles; // use rotation for speed
 
-		float speed = 5f;
+		
 
 		// move the rigidbody in the direction of the camera
 		rigidBody.MovePosition(transform.position + Camera.main.gameObject.transform.forward * speed * Time.deltaTime);
+        transform.position= new Vector3(transform.position.x, 1f, transform.position.z);
 
 	}
 }
