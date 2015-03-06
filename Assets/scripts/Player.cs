209203145bf44ca89b5971e597b7c0ca62505787
FocusBlast <me@manshantsingh.com>
2015-03-06 15:25:58 -0800

    In Progress
    +added start scene which starts the game with trigger
    +added 3D text directionChange which shows change when direction changes
    +added changing path change.
    
    ***the path change is not completely fixed and in progress***

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -7,10 +7,19 @@ public class Player : MonoBehaviour {
 
 	Rigidbody rigidBody;
 
-    public float speed;
+    public float speed, directionOffset, rotationSpeed;
+
+    public GameObject head,text;
+
+    TextMesh theText;
+    float lastRotationState;
+    Quaternion goalDirection;
 	
 	void Start () {
 		rigidBody = this.GetComponent<Rigidbody> ();
+        theText = text.gameObject.GetComponent<TextMesh>();
+        lastRotationState = head.transform.eulerAngles.y;
+        goalDirection = transform.rotation;
 	}
 
 	void FixedUpdate () {
@@ -18,7 +27,37 @@ public class Player : MonoBehaviour {
 		//Vector3 headRotation = this.transform.FindChild ("Head").rotation.eulerAngles; // use rotation for speed
 
 		// move the rigidbody in the direction of the camera
-		rigidBody.MovePosition(transform.position + Camera.main.gameObject.transform.forward * speed * Time.deltaTime);
+		rigidBody.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
 
+        float currentRotationState = head.transform.eulerAngles.y;
+
+
+        if (Mathf.Abs(currentRotationState - lastRotationState) > 180f)
+        {
+            if (currentRotationState > lastRotationState) lastRotationState += 360f;
+            else lastRotationState -= 360f;
+        }
+
+        //if (Mathf.Abs(currentRotationState - lastRotationState) > directionOffset && enableChange)
+        if (Mathf.Abs(currentRotationState - lastRotationState) > directionOffset)
+        {
+            goalDirection *= currentRotationState > lastRotationState ? Quaternion.Euler(0, 90, 0) : Quaternion.Euler(0, -90, 0);
+            print(lastRotationState + " vs " + currentRotationState);
+            theText.fontSize = 200;
+            //enableChange = false;
+        }
+        else if (theText.fontSize > 1) theText.fontSize--;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, goalDirection, rotationSpeed);
+        //transform.rotation = goalDirection;
+
+        //at the end
+        lastRotationState = head.transform.eulerAngles.y;
 	}
+
+    //bool enableChange = true;
+    //void Update()
+    //{
+    //    if (Cardboard.SDK.CardboardTriggered||Input.GetKeyDown(KeyCode.A)) enableChange = true;
+    //}
 }
---
+++ Assets/scripts/StartScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour {
+    
+    void Update()
+    {
+        if (Cardboard.SDK.CardboardTriggered) Application.LoadLevel(1);
+    }
+}
