846c3740ac76a918266eeffb90ff6b400bf1a04b
FocusBlast <me@manshantsingh.com>
2015-03-15 17:34:09 -0700

    Path Changer Update
    +no unnecessary paths around.
    +path changer works better than before (still not perfect).
    +trigger magnet to restart.
    
    -the rotation changes suddenly and therefore looks unnatural (I will
    deal with this later).

--- Assets/scripts/PathCreator.cs
+++ Assets/scripts/PathCreator.cs
@@ -7,19 +7,22 @@ public class PathCreator : MonoBehaviour {
 	string[] pathNames = new string[] {"Straight", "Left", "Right"};
 
 	List<GameObject> instantiatedObjects = new List<GameObject>(); // keeps track of all our path obejcts
+    int destroyIndex = -2;
 
 	void Start () {
 
 		//add the first one @index: 0
 		instantiatedObjects.Add(Instantiate(Resources.Load("Straight"), Vector3.zero, Quaternion.identity) as GameObject);
 
-        for (int i = 1; i < 5; i++) createNext();
+        //for (int i = 1; i < 5; i++) 
+        createNext();
 
 	}
 
 	public void createNext()
     {
-        string name = pathNames[Random.Range(0, pathNames.Length)];
+        int index = Random.Range(0, pathNames.Length);
+        string name = pathNames[index];
 
         Transform prevObj = instantiatedObjects[instantiatedObjects.Count-1].transform.FindChild("_end").transform;
 
@@ -27,9 +30,25 @@ public class PathCreator : MonoBehaviour {
                                      prevObj.position,
                                      prevObj.rotation) as GameObject;
         instantiatedObjects.Add(obj);
+
+        if (index == 0) 
+        {
+            createNext();
+        }
     }
     public void destroyOld()
     {
-        Destroy(instantiatedObjects[instantiatedObjects.Count - 6]);
+        if (destroyIndex < 0) destroyIndex++;
+        else if (instantiatedObjects[destroyIndex].tag == "Straight")
+        {
+            Destroy(instantiatedObjects[destroyIndex]);
+            destroyIndex++;
+            destroyOld();
+        }
+        else
+        {
+            Destroy(instantiatedObjects[destroyIndex]);
+            destroyIndex++;
+        }
     }
 }
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -15,19 +15,19 @@ public class Player : MonoBehaviour {
 
     float lastRotationState, currentRotationState;
     Quaternion goalDirection;
-	
+
 	void Start () {
 		rigidBody = this.GetComponent<Rigidbody> ();
         lastRotationState = head.transform.eulerAngles.y;
         goalDirection = transform.rotation;
 	}
 
-	void FixedUpdate () {
-
-		// move the rigidbody forward
-		rigidBody.MovePosition(transform.position + transform.forward * speed);
-        print(transform.forward * speed);
-
+    void Update()
+    {
+        if (Cardboard.SDK.CardboardTriggered) Application.LoadLevel(0);
+    }
+	void FixedUpdate ()
+    {
         currentRotationState = head.transform.eulerAngles.y;
 
         if (Mathf.Abs(currentRotationState - lastRotationState) > 180f)
@@ -38,30 +38,64 @@ public class Player : MonoBehaviour {
 
         if (text.fontSize > 1) text.fontSize--;
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, goalDirection, rotationSpeed);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, goalDirection, rotationSpeed);
+        transform.rotation = goalDirection;
 
         //at the end
         lastRotationState = head.transform.eulerAngles.y;
+
+        // move the rigidbody forward
+        print("FixedUpdate Position: " + transform.position);
+        rigidBody.MovePosition(transform.position + transform.forward * speed);
+        print("FixedUpdate Forward: " + transform.forward * speed);
 	}
 
     void OnTriggerStay(Collider other)
     {
-        print(other.tag);
+        
         if (other.tag == "Left" && currentRotationState - lastRotationState < -directionOffset)
         {
-            goalDirection *= Quaternion.Euler(0, -90, 0);
-            other.GetComponent<BoxCollider>().isTrigger = false;
-            text.fontSize = 200;
-            PathCreater_Script.createNext();
-            PathCreater_Script.destroyOld();
+            DirectionChangeCommon(other, -1);
         }
         else if (other.tag == "Right" && currentRotationState - lastRotationState > directionOffset)
         {
-            goalDirection *= Quaternion.Euler(0, 90, 0);
-            other.GetComponent<BoxCollider>().isTrigger = false;
-            text.fontSize = 200;
-            PathCreater_Script.createNext();
-            PathCreater_Script.destroyOld();
+            DirectionChangeCommon(other, 1);
         }
     }
+    void DirectionChangeCommon(Collider other, int i)
+    {
+        print(other.tag);
+        goalDirection *= Quaternion.Euler(0, 90 * i, 0);
+        other.GetComponent<BoxCollider>().isTrigger = false;
+        text.fontSize = 200;
+        PathCreater_Script.createNext();
+        PathCreater_Script.destroyOld();
+        int dirY = Mathf.RoundToInt(goalDirection.eulerAngles.y);
+        Vector3 pos = transform.position;
+
+        //i = 0;
+        print("****************************************IT'S OVER HERE****************************************");
+        print("Current Direction: " + dirY % 180);
+        print("collider position:  x=" + other.transform.position.x + "  z=" + other.transform.position.z);
+
+        if (dirY % 180 == 0)
+        {
+            pos.x = other.transform.position.x;
+        }
+        else if (dirY % 180 == 90)
+        {
+            pos.z = other.transform.position.z;
+        }
+        //else if (dirY % 360 == 180)
+        //{
+        //    pos.x = other.transform.position.x;
+        //}
+        //else
+        //{
+        //    pos.z = other.transform.position.z;
+        //}
+        transform.position = pos;
+        print("position of player: " + transform.position);
+    }
 }
+
